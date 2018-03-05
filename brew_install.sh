d64f69751a682ef8fa89bd334886e684be94284e
Tuấn Vương <vanhtuan0409@gmail.com>
2018-03-05 12:53:36 +0700

    add setup for brew

---
+++ brew_install.sh
@@ -0,0 +1,9 @@
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if [ -f brew.txt ]; then
+    xargs brew install < brew.txt
+fi
