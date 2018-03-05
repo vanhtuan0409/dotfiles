6e2ce62a264480b351ece67bc46bf3f6ca471fac
Tuấn Vương <vanhtuan0409@gmail.com>
2018-03-05 13:17:44 +0700

    update brew install script

--- brew_install.sh
+++ brew_install.sh
@@ -2,8 +2,8 @@ which -s brew
 if [[ $? != 0 ]] ; then
     # Install Homebrew
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [ -f brew.txt ]; then
+        xargs brew install < brew.txt
+    fi
 fi
 
-if [ -f brew.txt ]; then
-    xargs brew install < brew.txt
-fi
