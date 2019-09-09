e08c7c6846391f3337ae44aea69a437380ccb621
Tuan Vuong <vanhtuan0409@gmail.com>
2019-09-09 10:40:07 +0700

    auto detect and generate image for i3lock

--- scripts/generate_wallpaper.sh
+++ scripts/generate_wallpaper.sh
@@ -1,3 +1,21 @@
-convert $DOTFILES/resources/wallpaper.jpg -resize 1600x900\> wallpaper_1600x900.jpg
-convert $DOTFILES/resources/wallpaper.jpg -resize 1920x1080\> wallpaper_1920x1080.jpg
-convert $DOTFILES/resources/wallpaper_1600x900.jpg $DOTFILES/resources/wallpaper_1920x1080.jpg $DOTFILES/resources/wallpaper_1920x1080.jpg +append $DOTFILES/resources/wallpaper_tripple.jpg
+original_wallpaper=$1
+full_filename=$(basename -- "$original_wallpaper")
+extension="${full_filename##*.}"
+filename="${full_filename%.*}"
+
+echo "Processing $full_filename"
+
+mkdir -p /tmp/i3_wallpaper
+res=$(xrandr --query | grep "\*" | awk '{print $1}')
+tmp_imgs=()
+for item in $res
+do
+  echo "Resizing to resolution $item"
+  convert $original_wallpaper -resize $item\> /tmp/i3_wallpaper/$item.$extension
+  tmp_imgs+=(/tmp/i3_wallpaper/$item.$extension)
+done
+
+output_wallpaper="${filename}_merged.${extension}"
+echo "Merging images to $output_wallpaper"
+IFS=" "
+convert ${tmp_imgs[*]} +append $output_wallpaper
