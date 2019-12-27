6de7ff50c3ad50d0625081d2adbe80150d4c212e
Tuan Vuong <vanhtuan0409@gmail.com>
2019-12-27 12:19:23 +0700

    rename output

--- scripts/autoxrandr.sh
+++ scripts/autoxrandr.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-laptop=eDP1
-vga=DP1
-hdmi=HDMI1
-virtual=VIRTUAL1
+laptop=eDP-1
+vga=DP-1
+hdmi=HDMI-1
 
 xrandr --auto --output $laptop --primary --mode 1600x900 --rotate normal --rate 60
 if xrandr | grep "$hdmi connected"; then
