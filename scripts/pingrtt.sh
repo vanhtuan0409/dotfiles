ba3a5c348973d1ef2df2d0b7b7f5159c49e0a069
Tuan Vuong <vanhtuan0409@gmail.com>
2019-12-28 13:12:39 +0700

    format color

--- scripts/pingrtt.sh
+++ scripts/pingrtt.sh
@@ -8,12 +8,12 @@ else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
     if [ "$rtt" -lt 50 ]; then
-        icon="%{F#3cb703}  %{F-}"
+        icon="%{F#3cb703}  "
     elif [ "$rtt" -lt 150 ]; then
-        icon="%{F#f9dd04}  %{F-}"
+        icon="%{F#f9dd04}  "
     else
-        icon="%{F#d60606}  %{F-}"
+        icon="%{F#d60606}  "
     fi
 
-    echo "$icon $rtt ms"
+    echo "$icon $rtt ms%{F-}"
 fi
