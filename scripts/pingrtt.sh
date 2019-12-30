7e05129eca58e26216480536b9d8f80df9f2a62d
Tuan Vuong <vanhtuan0409@gmail.com>
2019-12-30 09:28:04 +0700

    updated

--- scripts/pingrtt.sh
+++ scripts/pingrtt.sh
@@ -6,5 +6,5 @@ if ! ping=$(ping -n -c 1 -W 1 $HOST 2> /dev/null); then
     echo ""
 else
     rtt=$(echo "$ping" | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
-    echo "  $rtt ms"
+    echo "  $rtt ms"
 fi
