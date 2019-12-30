8538f935c78ce42f4314f7e5ae72d379da8b0eaa
Tuan Vuong <vanhtuan0409@gmail.com>
2019-12-30 13:59:28 +0700

    refactor

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -5,11 +5,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-
-TRAY_POSITION=right polybar primary -c $HOME/dotfiles/config/polybar/config &
-TRAY_POSITION=right polybar bottom -c $HOME/dotfiles/config/polybar/config &
-
-for m in $(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1); do
+for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
   MONITOR=$m polybar primary -c $HOME/dotfiles/config/polybar/config &
   MONITOR=$m polybar bottom -c $HOME/dotfiles/config/polybar/config &
 done
