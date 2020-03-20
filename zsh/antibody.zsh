34829a6e77b477937e8e6d0d022b6ef85776786c
Tuan Vuong <vanhtuan0409@gmail.com>
2020-03-20 14:21:25 +0700

    private out some function

--- zsh/antibody.zsh
+++ zsh/antibody.zsh
@@ -1,36 +1,35 @@
-antibody_load() {
+_antibody_load() {
   antibody bundle $@ >> $ANTIBODY_CACHE_FILE
 }
 
-antibody_plugins() {
+_antibody_plugins() {
+  local _zsh_init="""
+autoload -Uz compaudit compinit
+# Avoid checking zcompdump every time
+for dump in ~/.zcompdump(N.mh+24); do
+  compinit
+done
+
+compinit -C
+  """
+
   echo "" > $ANTIBODY_CACHE_FILE
-  antibody_load robbyrussell/oh-my-zsh kind:fpath path:plugins/cargo
-  echo "zsh_init" >> $ANTIBODY_CACHE_FILE
-  antibody_load robbyrussell/oh-my-zsh path:lib/clipboard.zsh
-  antibody_load robbyrussell/oh-my-zsh path:lib/completion.zsh
-  antibody_load robbyrussell/oh-my-zsh path:lib/git.zsh
-  antibody_load robbyrussell/oh-my-zsh path:lib/history.zsh
-  antibody_load robbyrussell/oh-my-zsh path:lib/spectrum.zsh
-  antibody_load robbyrussell/oh-my-zsh path:lib/theme-and-appearance.zsh
-  antibody_load zsh-users/zsh-autosuggestions
-  antibody_load Tarrasch/zsh-bd
-  # antibody_load iridakos/goto
-  antibody_load vanhtuan0409/goto # temporary using a fork for patching bug
-  antibody_load superbrothers/zsh-kubectl-prompt
-}
-
-zsh_init() {
-  autoload -Uz compaudit compinit
-  # Avoid checking zcompdump every time
-  for dump in ~/.zcompdump(N.mh+24); do
-    compinit
-  done
-
-  compinit -C
+  _antibody_load robbyrussell/oh-my-zsh kind:fpath path:plugins/cargo
+  echo "$_zsh_init" >> $ANTIBODY_CACHE_FILE
+  _antibody_load robbyrussell/oh-my-zsh path:lib/clipboard.zsh
+  _antibody_load robbyrussell/oh-my-zsh path:lib/completion.zsh
+  _antibody_load robbyrussell/oh-my-zsh path:lib/git.zsh
+  _antibody_load robbyrussell/oh-my-zsh path:lib/history.zsh
+  _antibody_load robbyrussell/oh-my-zsh path:lib/spectrum.zsh
+  _antibody_load robbyrussell/oh-my-zsh path:lib/theme-and-appearance.zsh
+  _antibody_load zsh-users/zsh-autosuggestions
+  _antibody_load Tarrasch/zsh-bd
+  _antibody_load iridakos/goto
+  _antibody_load superbrothers/zsh-kubectl-prompt
 }
 
 antibody_refresh() {
   rm -rf $ANTIBODY_CACHE_FILE
   rm -rf ~/.zcompdump
-  antibody_plugins
+  _antibody_plugins
 }
