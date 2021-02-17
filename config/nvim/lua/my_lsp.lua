f24dab81a87637dc07bdd7bc4533c5d84b1b6e76
Tuan Vuong <vanhtuan0409@gmail.com>
2021-02-17 10:24:02 +0700

    update

--- config/nvim/lua/my_lsp.lua
+++ config/nvim/lua/my_lsp.lua
@@ -6,6 +6,10 @@ require'lspsaga'.init_lsp_saga {
   warn_sign = '',
   hint_sign = '',
   infor_sign = '',
+  dianostic_header_icon = '   ',
+  code_action_icon = '',
+  finder_definition_icon = '* ',
+  finder_reference_icon = '* ',
   finder_action_keys = {
     quit = '<Esc>'
   },
