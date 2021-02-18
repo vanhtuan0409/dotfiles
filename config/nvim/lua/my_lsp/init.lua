0aac2dc349f94cb12cdc0556f5afa19414cd0969
Tuan Vuong <vanhtuan0409@gmail.com>
2021-02-18 10:39:14 +0700

    refactor

---
+++ config/nvim/lua/my_lsp/init.lua
@@ -0,0 +1,113 @@
+require'my_lsp/extensions'
+local lspconfig = require'lspconfig'
+local lsp_util = require'lspconfig/util'
+local lsp_mappings = require'my_lsp/mappings'
+
+require'lspsaga'.init_lsp_saga {
+  error_sign = '',
+  warn_sign = '',
+  hint_sign = '',
+  infor_sign = '',
+  dianostic_header_icon = '   ',
+  code_action_icon = ' ',
+  finder_definition_icon = '* ',
+  finder_reference_icon = '* ',
+  finder_action_keys = {
+    quit = '<Esc>'
+  },
+  code_action_keys = {
+    quit = '<Esc>'
+  },
+  rename_action_keys = {
+    quit = '<Esc>'
+  }
+}
+
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = false;
+    vsnip = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+    spell = false;
+    tags = false;
+    snippets_nvim = true;
+    treesitter = true;
+  };
+}
+
+-- Setup mapping completions
+lsp_mappings.setup_completion()
+
+-- LSP on attach
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_command([[autocmd CursorHold  * call v:lua.custom_show_line_diagnostics({ "show_header": v:false })]])
+  lsp_mappings.setup_on_attach(client, bufnr)
+
+  -- auto format
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_exec([[
+      augroup Format
+        autocmd! * <buffer>
+        autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()
+      augroup END
+    ]], false)
+  end
+end
+
+-- remove default format of lang server
+local on_attach_no_format = function(client, bufnr)
+  client.resolved_capabilities.document_formatting = false
+  on_attach(client, bufnr)
+end
+
+-- customize diagnostic info
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = false,
+    -- Hide virtual text
+    virtual_text = false,
+    -- Increase diagnostic signs priority
+    signs = {
+      priority = 9999,
+    },
+    update_in_insert = false,
+  }
+)
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.gopls.setup{
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = { "/home/tuan/.config/coc/extensions/coc-go-data/bin/gopls" },
+  settings = {
+    gopls = {
+      usePlaceholders = true,
+      completeUnimported = true,
+    }
+  }
+}
+
+lspconfig.rust_analyzer.setup{
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = { "/home/tuan/.config/coc/extensions/coc-rust-analyzer-data/rust-analyzer" },
+}
