9647e53456d694eba763c7117b577c435fb71b42
Tuan Vuong <vanhtuan0409@gmail.com>
2021-03-16 13:21:20 +0700

    refactor

--- config/nvim/lua/my_lsp/init.lua
+++ config/nvim/lua/my_lsp/init.lua
@@ -1,94 +1,9 @@
-require'my_lsp/extensions'
 local lspconfig = require'lspconfig'
-local lsp_util = require'lspconfig/util'
-local lsp_status = require'lsp-status'
-local lsp_mappings = require'my_lsp/mappings'
+local on_attach = require'my_lsp/on_attach'
 
-require'lspsaga'.init_lsp_saga {
-  error_sign = '',
-  warn_sign = '',
-  hint_sign = '',
-  infor_sign = '',
-  dianostic_header_icon = '   ',
-  code_action_icon = ' ',
-  finder_definition_icon = '* ',
-  finder_reference_icon = '* ',
-  finder_action_keys = {
-    quit = '<Esc>'
-  },
-  code_action_keys = {
-    quit = '<Esc>'
-  },
-  rename_action_keys = {
-    quit = '<Esc>'
-  }
-}
-
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
-
-  source = {
-    path = true;
-    buffer = true;
-    calc = false;
-    vsnip = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    spell = false;
-    tags = false;
-    snippets_nvim = true;
-    treesitter = true;
-  };
-}
-
-lsp_status.config {
-  current_function = false,
-  status_symbol = '',
-  indicator_errors = '',
-  indicator_warnings = '',
-  indicator_info = '',
-  indicator_hint = '',
-}
-lsp_status.register_progress()
-
--- Setup mapping completions
-lsp_mappings.setup_completion()
-
--- LSP on attach
-local on_attach = function(client, bufnr)
-  lsp_status.on_attach(client, bufnr)
-
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-  vim.api.nvim_command([[autocmd CursorHold  * call v:lua.custom_show_line_diagnostics({ "show_header": v:false })]])
-  lsp_mappings.setup_on_attach(client, bufnr)
-
-  -- auto format
-  if client.resolved_capabilities.document_formatting then
-    vim.api.nvim_exec([[
-      augroup Format
-        autocmd! * <buffer>
-        autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()
-      augroup END
-    ]], false)
-  end
-end
-
--- remove default format of lang server
-local on_attach_no_format = function(client, bufnr)
-  client.resolved_capabilities.document_formatting = false
-  on_attach(client, bufnr)
-end
+require'my_lsp/plugins/saga'
+require'my_lsp/plugins/compe'
+require'my_lsp/plugins/status'
 
 -- customize diagnostic info
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
@@ -104,12 +19,19 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
+-- Set snippet support and status line compatibilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = vim.tbl_extend('keep', capabilities, lsp_status.capabilities)
+capabilities = vim.tbl_extend('keep', capabilities, require'lsp-status'.capabilities)
+lspconfig.util.default_config = vim.tbl_extend(
+  "force",
+  lspconfig.util.default_config,
+  {
+    capabilities = capabilities
+  }
+)
 
 lspconfig.gopls.setup{
-  capabilities = capabilities,
   on_attach = on_attach,
   cmd = { "/home/tuan/.config/coc/extensions/coc-go-data/bin/gopls" },
   settings = {
@@ -121,7 +43,6 @@ lspconfig.gopls.setup{
 }
 
 lspconfig.rust_analyzer.setup{
-  capabilities = capabilities,
   on_attach = on_attach,
   cmd = { "/home/tuan/.config/coc/extensions/coc-rust-analyzer-data/rust-analyzer" },
 }
---
+++ config/nvim/lua/my_lsp/on_attach.lua
@@ -0,0 +1,32 @@
+-- LSP on attach
+local on_attach = function(client, bufnr)
+  require'lsp-status'.on_attach(client, bufnr)
+
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function command(...) vim.api.nvim_command(...) end
+
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  command([[autocmd CursorHold  * call v:lua.custom_show_line_diagnostics({ "show_header": v:false })]])
+
+  -- mappings
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'K', [[:Lspsaga hover_doc<CR>]], opts)
+  buf_set_keymap('n', 'gh', [[:Lspsaga lsp_finder<CR>]], opts)
+  buf_set_keymap('n', '<leader>rn', [[:Lspsaga rename<CR>]], opts)
+  buf_set_keymap('n', '<leader>ga', [[:Lspsaga code_action<CR>]], opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<leader>d', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+
+  -- auto format
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_exec([[
+      augroup Format
+        autocmd! * <buffer>
+        autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()
+      augroup END
+    ]], false)
+  end
+end
+
+return on_attach
