d6c60affceee066e756ada60e3089eea15e568f7
Tuan Vuong <vanhtuan0409@gmail.com>
2021-04-20 10:48:35 +0700

    remove timestamp

--- config/nvim/lua/modules/debugging/init.lua
+++ config/nvim/lua/modules/debugging/init.lua
@@ -18,15 +18,12 @@ vim.fn.sign_define('DapBreakpoint', {
 
 require 'modules/debugging/adapters/go'
 
-local mapper = function(mode, key, result)
-  vim.api.nvim_set_keymap(mode, key, result, {noremap = true, silent = true})
-end
-
-mapper('n', '<F1>', '<cmd>lua require\'dap\'.disconnect()<CR>')
-mapper('n', '<F5>', '<cmd>lua require\'dap\'.continue()<CR>')
-mapper('n', '<F9>', '<cmd>lua require\'dap\'.toggle_breakpoint()<CR>')
-mapper('n', '<F10>', '<cmd>lua require\'dap\'.step_over()<CR>')
-mapper('n', '<F11>', '<cmd>lua require\'dap\'.step_into()<CR>')
-mapper('n', '<F12>', '<cmd>lua require\'dap\'.step_out()<CR>')
+local opts = {noremap = true, silent = true}
+vim.api.nvim_set_keymap('n', '<F1>', '<cmd>lua require\'dap\'.disconnect()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<F5>', '<cmd>lua require\'dap\'.continue()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<F9>', '<cmd>lua require\'dap\'.toggle_breakpoint()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<F10>', '<cmd>lua require\'dap\'.step_over()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<F11>', '<cmd>lua require\'dap\'.step_into()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<F12>', '<cmd>lua require\'dap\'.step_out()<CR>', opts)
 
 require('dap.ext.vscode').load_launchjs()
