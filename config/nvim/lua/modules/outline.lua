9be28662bd23799092e713b38334b85624000ba3
Tuan Vuong <vanhtuan0409@gmail.com>
2021-07-09 09:50:05 +0700

    updates

--- config/nvim/lua/modules/outline.lua
+++ config/nvim/lua/modules/outline.lua
@@ -1,4 +1,5 @@
 vim.g.symbols_outline = {
+  highlight_hovered_item = false,
   auto_preview = false,
 }
 vim.api.nvim_set_keymap("n", "<leader>o", ":SymbolsOutline<cr>", { silent=true, noremap = true})
