c98e2592f86a3001262fd110e359cfa878241eaa
Tuan Vuong <vanhtuan0409@gmail.com>
2022-03-01 09:25:03 +0700

    update

--- scripts/startkeyring.sh
+++ scripts/startkeyring.sh
@@ -3,4 +3,6 @@
 # use to automatically unlock keyring with password stored in a file
 # in case dont want the keyring password to be empty
 # or using fprintd
-echo -n $(cat $HOME/.config/keyringpass) | /usr/bin/gnome-keyring-daemon --daemonize --components=gpg,pkcs11,secrets,ssh --unlock
+# NOTED: you should disable `auto_start` option in /etc/pamd.d/lightdm-*
+echo -n $(cat $HOME/.config/keyringpass) | /usr/bin/gnome-keyring-daemon --daemonize --unlock --replace
+/usr/bin/gnome-keyring-daemon --start
