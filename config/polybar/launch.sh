98b0c3b7d9d3a72c175159c2b283a8940f69a92e
Tuan Vuong <vanhtuan0409@gmail.com>
2022-04-05 09:20:08 +0700

    update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -5,7 +5,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+for m in $(xrandr --query | grep " connected" | grep " primary" | cut -d" " -f1); do
   MONITOR=$m polybar primary -c $HOME/dotfiles/config/polybar/config &
   MONITOR=$m polybar bottom -c $HOME/dotfiles/config/polybar/config &
 done
