296332926c90355cbaf6083cb9bb2917406f3425
Tuan Vuong <vanhtuan0409@gmail.com>
2022-04-14 14:40:07 +0700

    update

--- config/nvim/lua/modules/themes.lua
+++ config/nvim/lua/modules/themes.lua
@@ -1,15 +1,16 @@
 local M = {}
 
 function M.gruvbox()
-  vim.g.background = 'dark'
+  vim.opt.background = 'dark'
   vim.g.gruvbox_material_enable_italic = true
   vim.g.gruvbox_material_better_performance = true
   vim.cmd('colorscheme gruvbox-material')
 end
 
 function M.everforest()
-  vim.g.background = 'dark'
+  vim.opt.background = 'dark'
   vim.g.everforest_background = 'hard'
+  vim.g.everforest_ui_contrast = 'high'
   vim.g.everforest_enable_italic = true
   vim.g.everforest_better_performance = true
   vim.cmd('colorscheme everforest')
