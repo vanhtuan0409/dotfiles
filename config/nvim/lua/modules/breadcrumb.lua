45f45d8cf93fb35398e1460e01782fbd8cf23ce4
Tuan Vuong <vanhtuan0409@gmail.com>
2022-07-11 09:53:09 +0700

    fix neovim warning

--- config/nvim/lua/modules/breadcrumb.lua
+++ config/nvim/lua/modules/breadcrumb.lua
@@ -1,3 +1,3 @@
-vim.highlight.link("BreadcrumbText", "LspCodeLens", false)
-vim.highlight.link("BreadcrumbSep", "WarningMsg", false)
+vim.api.nvim_set_hl(0, "BreadcrumbText", { link = "LspCodeLens", default = true })
+vim.api.nvim_set_hl(0, "BreadcrumbSep", { link = "WarningMsg", default = true })
 require'winbar-breadcrumb'.setup()
