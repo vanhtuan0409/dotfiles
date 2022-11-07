b365b54c4626b62df63f7599687a7dad4c8e7472
Tuan Vuong <vanhtuan0409@gmail.com>
2022-11-07 14:01:56 +0700

    fix overlapping diagnostic

--- config/nvim/lua/modules/lsp/ext/auto_diagnostic.lua
+++ config/nvim/lua/modules/lsp/ext/auto_diagnostic.lua
@@ -1,5 +1,12 @@
 local augroup = vim.api.nvim_create_augroup("LspDiagnostics", { clear = true })
 
+local function open_diagnostic(bufnr, opts)
+  local existing_float = vim.F.npcall(vim.api.nvim_buf_get_var, bufnr, 'lsp_floating_preview')
+  if not (existing_float and vim.api.nvim_win_is_valid(existing_float)) then
+    vim.diagnostic.open_float(opts)
+  end
+end
+
 return function(client, bufnr)
   local diagnostic_method = "textDocument/publishDiagnostics"
   if client.supports_method(diagnostic_method) then
@@ -8,7 +15,7 @@ return function(client, bufnr)
       group = augroup,
       buffer = bufnr,
       callback = function(params)
-        vim.diagnostic.open_float(nil)
+        open_diagnostic(bufnr, nil)
       end,
     })
   end
